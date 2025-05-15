e34bd03e4e7e1125b26da2f76b9e6e76a0422422
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-05-15 11:34:08 +0200

    fine esercizio senza bonus (per ora)

--- esercizi.js
+++ esercizi.js
@@ -39,6 +39,24 @@ async function getDashboardData(city) {
             weather: weather.weather_description,
             airport: airport.name
         };
+    
+
+        return result;
         
+    }catch (error) {
+        console.error('Errore durante il recupero dei dati della dashboard:', error.message);
+        throw error;
     }
 }
+
+
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
